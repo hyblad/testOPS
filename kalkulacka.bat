0ba5fe7b6607eeb95c89ad9603804eb92cfdf2bf
Hybl <HyblAd@vda.cz>
2025-06-06 11:33:27 +0200

    dokončení kalkulačky

--- kalkulacka.bat
+++ kalkulacka.bat
@@ -1,14 +1,41 @@
 @echo off
 
-set /p cislo1="Zadej prvni cislo: "
-set /p cislo2="Zadej druhe cislo: "
-set /p operace="Zadej operaci kterou chces pouzit(napis soucet, nasobeni nebo deleni): "
 
-if %operace%=="soucet" set /a %cislo1%+%cislo2%=vysledek 
-if %operace%=="nasobeni" set /a %cislo1%*%cislo2%=vysledek 
-if %operace%=="deleni" set /a %cislo1%/%cislo2%=vysledek 
+:start
+set /p cisloa="Zadej prvni cislo: "
+set /p cislob="Zadej druhe cislo: "
+set /p operace="Zadej operaci kterou chces pouzit (soucet, nasobeni, deleni nebo 0 pro konec): "
 
 
+if %operace%==0 ( goto konec 
+)
 
 
+
+if %operace%=="soucet"   ( 
+    set /a vysledek=%cisloa% + %cislob% 
+)
+echo soucet cisel %cisloa% a %cislob% je %vysledek%  
+goto start
+
+
+
+if %operace%=="nasobeni" (
+    set /a vysledek=%cisloa% * %cislob%
+)
+echo nasobek cisel %cisloa% a %cislob% je %vysledek% 
+goto start
+
+
+
+if %operace%=="deleni"   (
+     set /a vysledek=%cisloa% / %cislob% 
+)
+echo nasobek cisel %cisloa% a %cislob% je %vysledek% 
+goto start
+
+
+
+
+:konec
 pause
