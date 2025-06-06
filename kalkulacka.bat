194410e36df29d48b9ba3d0fdba66abf46b12bce
Hybl <HyblAd@vda.cz>
2025-06-06 10:27:25 +0200

    upravení programu pro správný výsledek

--- kalkulacka.bat
+++ kalkulacka.bat
@@ -2,12 +2,11 @@
 
 set /p cislo1="Zadej prvni cislo: "
 set /p cislo2="Zadej druhe cislo: "
-set /p operace="Zadej operaci kterou chces pouzit: "
+set /p operace="Zadej operaci kterou chces pouzit(napis soucet, nasobeni nebo deleni): "
 
-set /a %cislo1%%operace%%cislo2%=vysledek
-
-
-
+if %operace%=="soucet" set /a %cislo1%+%cislo2%=vysledek 
+if %operace%=="nasobeni" set /a %cislo1%*%cislo2%=vysledek 
+if %operace%=="deleni" set /a %cislo1%/%cislo2%=vysledek 
 
 
 
